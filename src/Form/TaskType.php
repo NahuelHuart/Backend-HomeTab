ea3adeb8251d9c2ebe6609226da8849510f88d20
SStringlord <a2409815@institutmontilivi.cat>
2026-01-03 23:41:51 +0100

    feat: make CRUD for Task and create twig and TaskController

---
+++ src/Form/TaskType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Household;
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('completed')
+            ->add('createdAt')
+            ->add('household', EntityType::class, [
+                'class' => Household::class,
+                'choice_label' => 'id',
+            ])
+            ->add('assignedTo', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
